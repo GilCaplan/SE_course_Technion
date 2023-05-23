e5fb23badb2760f4f3f734f95b86718809149da0
GilCaplan <rockycaplan@gmail.com>
2023-05-23 15:26:24 +0300

    more or less finished Part A. Amir go over it and make sure ti's good
    Part B- made classes

---
+++ src/PartB/Function.java
@@ -0,0 +1,35 @@
+package PartB;
+
+public class Function {
+
+    //receives x in R and returns functions value at the point
+    public int valueAt(int x){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public Function derivative(){
+        return null;
+    }
+    public int bisectionMethod(int a, int b, int epsilon){
+        return 0;
+    }
+    public int bisectionMethod(int a, int b){
+        double epsilon = 10^(-5);
+        return 0;
+    }
+    public int newtonRaphsonMethod(int a, int epsilon){
+        return 0;
+    }
+    public int newtonRaphsonMethod(int a){
+        double epsilon = 10^(-5);
+        return 0;
+    }
+    public Function taylorPolynomial(int n){
+        return null;
+    }
+}
---
+++ src/PartB/MultiSum.java
@@ -0,0 +1,4 @@
+package PartB;
+
+public class MultiSum {
+}

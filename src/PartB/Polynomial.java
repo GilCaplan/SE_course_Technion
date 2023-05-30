9db88dee3f725ab8c5bb6a95c0d91b05beb3af38
GilCaplan <rockycaplan@gmail.com>
2023-05-31 00:19:04 +0300

    Finished writing code.
    overall code works, no errors. maybe fix formatting a bit to match what they want and look at some more specific stuff.
    
    few more format fixes needed.
    
    doesn't seem to manage to do the last root searches, so need to check on that

--- src/PartB/Polynomial.java
+++ src/PartB/Polynomial.java
@@ -9,40 +9,11 @@ public class Polynomial extends Function{
         this.functions[0] = new Constant(a1);
         this.taylor = taylor;
     }
-    public Polynomial(boolean taylor, double... an){
-        this.taylor = taylor;
-        int cnt=0;
-        //cnt how many none 0's there are
-        for (double v : an) if (v != 0) cnt++;
-        this.functions = new Function[cnt];
-        int j=0;
-
-        if (an[0] != 0) {
-            this.functions[j] = new Power(new X(an[0]), 0);
-            j++;
-        }
-
-        for(int i=1; i< an.length; i++) {
-            if (an[i] != 0) {
-                this.functions[j] = new Power(new X(an[i]), i);
-                j++;
-            }
-        }
-    }
     public Polynomial(boolean taylor, Function[] functions){
         this.taylor = taylor;
         this.functions = new Function[functions.length];
-        for(int i=0; i<functions.length; i++){
-            if(functions[i] instanceof Power && ((Power)functions[i]).getF() instanceof X) {
-                double check = ((X) ((Power) functions[i]).getF()).getNum();
-                if (check < 0)
-                    this.functions[i] = new Negation(new Power(new X(abs(check)), ((Power) functions[i]).getN()));
-                else
-                    this.functions[i] = functions[i];
-            }
-            else
-                this.functions[i] = functions[i];
-        }
+        for(int i=0; i<functions.length; i++)
+            this.functions[i] = functions[i];
     }
     public Polynomial(double... an){
         this.taylor = false;
@@ -85,10 +56,13 @@ public class Polynomial extends Function{
         if(functions.length == 1)
             return functions[0].toString();
         StringBuilder fStr = new StringBuilder() ;
-        for (Function function : functions)
-            fStr.append(function.toString()).append(" + ");
+        for (Function function : functions) {
+            if(function.toString().contains("-") && fStr.length() > 2)
+                fStr = new StringBuilder(fStr.substring(0, fStr.length()-2));
+             fStr.append(function).append(" + ");
+        }
         if(this.taylor)
-            return "(" + fStr.substring(0, fStr.length() - 3).replaceAll("[()]", "") + ")";
+            return "(" + fStr.substring(0, fStr.length() - 3).replaceAll("[()]", "").replaceAll(" -"," - ") + ")";
         return "(" + fStr.substring(0, fStr.length() - 3) + ")";
     }
 
